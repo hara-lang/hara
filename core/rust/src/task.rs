a9008e396c2bd725a5f1623bc7047e2ed20aa6b4
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-14 03:32:34 +1000

    Expose the production analyzer module

--- core/rust/src/task.rs
+++ core/rust/src/task.rs
@@ -1,4 +1,7 @@
 #[path = "task/promise.rs"]
 pub mod promise;
 
+#[path = "project/production.rs"]
+pub mod production;
+
 pub use promise::{LocalPromiseProvider, Promise, PromiseProvider, PromiseRejection, PromiseState};
