fa78b686eb41310477b6669e3709bdb023c79246
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-21 09:24:56 +1000

    Make Hara expression nodes instrumentable

--- core/java/src/main/java/hara/truffle/node/HaraExpressionNode.java
+++ core/java/src/main/java/hara/truffle/node/HaraExpressionNode.java
@@ -1,16 +1,46 @@
 package hara.truffle.node;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.instrumentation.GenerateWrapper;
+import com.oracle.truffle.api.instrumentation.InstrumentableNode;
+import com.oracle.truffle.api.instrumentation.ProbeNode;
+import com.oracle.truffle.api.instrumentation.StandardTags;
+import com.oracle.truffle.api.instrumentation.Tag;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.SourceSection;
+import hara.truffle.HaraInstrumentationTags;
 
-public abstract class HaraExpressionNode extends Node {
+/** Base expression node with generated Truffle instrumentation wrappers. */
+@GenerateWrapper
+public abstract class HaraExpressionNode extends Node implements InstrumentableNode {
   private SourceSection sourceSection;
+  private boolean executionRoot;
 
   public abstract Object execute(VirtualFrame frame);
 
   public void setHaraSourceSection(SourceSection sourceSection) {
     this.sourceSection = sourceSection;
+  }
+
+  void markExecutionRoot(SourceSection fallback) {
+    executionRoot = true;
+    if (sourceSection == null) sourceSection = fallback;
+  }
+
+  @Override
+  public boolean isInstrumentable() {
+    return sourceSection != null || executionRoot;
+  }
+
+  @Override
+  public WrapperNode createWrapper(ProbeNode probe) {
+    return new HaraExpressionNodeWrapper(this, probe);
+  }
+
+  @Override
+  public boolean hasTag(Class<? extends Tag> tag) {
+    return tag == StandardTags.ExpressionTag.class
+        || (executionRoot && tag == HaraInstrumentationTags.ExecutionRootTag.class);
   }
 
   @Override
